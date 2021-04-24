c93445ff5d0f0c9e53b1c4d971fd4987952e05b3
mahfuj890 <mahfujali490@gmail.com>
2021-04-25 02:07:01 +0600

    First Commit

---
+++ assets/JS/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+  //  sticky navbar
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+      $('.navbar_wrapper ').addClass("navbar-fixed");
+    } else {
+      $('.navbar_wrapper ').removeClass("navbar-fixed");
+    }
+
+  });
+  $(".bar_line").click(function () {
+    $(this).hide();
+    $(".times_icon").show();
+    $(".right_shapes").hide();
+
+  });
+  $(".times_icon").click(function () {
+    $(this).hide();
+    $(".bar_line").show();
+    $(".right_shapes").show();
+
+
+  });
+
+  // Multi Level Nested Dropdown and Navbar
+  $("#navbar").bootnavbar();
+  $("#home_current").mouseenter(function () {
+
+
+  });
+
+
+
+  // Waypoint For Counter
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1200
+  });
+
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+
+});
+// For Video Pause after closing
+var myVideo = document.getElementById("video_btn_close");
+
+function playPause() {
+  if (myVideo.play)
+    myVideo.pause();
+
+
+}
+// Slider Swiper JS
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+});
+
+//Tesimonial Swiper JS
+var swiper = new Swiper('.swiper-container2', {
+
+  navigation: {
+    nextEl: '.fa-arrow-right',
+    prevEl: '.fa-arrow-left',
+  },
+});
+
+// AOS On Page Scroll JS
+AOS.init({
+  duration: "1100",
+  offest: "50"
+});
