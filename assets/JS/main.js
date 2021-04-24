777d2c0ed0095e463b9719031b10d59b82b969f1
mahfuj890 <mahfujali490@gmail.com>
2021-04-25 02:33:36 +0600

    fix image  issue

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -38,22 +38,7 @@ $(document).ready(function () {
   });
 
 
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 1
-      }
-    }
-  });
+
 
 
 });
